7fdef7393ddefa85d0979a4102e731f8587399bb
Geunyeong Yu <i517099@fhict.nl>
2023-06-03 14:43:48 +0900

    Initial commit

---
+++ src/main/java/com/project/teamproject/TeamprojectApplication.java
@@ -0,0 +1,13 @@
+package com.project.teamproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamprojectApplication.class, args);
+	}
+
+}
