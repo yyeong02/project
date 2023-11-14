e9b4982bb6cbe903144f1ffbbda4084e08903eb8
haileykim12 <annaian@naver.com>
2023-11-15 08:07:56 +0900

    update responsive
    다른 창들 반응형 지원, 헤더 오류 수정 등등

--- src/main/resources/static/js/includeHTML.js
+++ src/main/resources/static/js/includeHTML.js
@@ -1,17 +1,16 @@
-function loadHTML(file)
-{
-    var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function ()
-    {
-        if(rawFile.readyState === 4)
-        {
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
-                var allText = rawFile.responseText;
-                document.write(allText);
-            }
-        }
-    }
-    rawFile.send(null);
-}
+var mainheader=`
+
+<table>
+    <th>    
+        <td><a type="button" id="btnHome" onclick="location.href='/home/'">HOME </a></td>
+        <td><a  id="btnHomeToCalendar" type="button" onclick="location.href='/calendar/'">달력</a></t>
+        <td><a  id="btnHomeToMedicineBox" type="button" onclick="location.href='/medicineBox/'">지도-수거함</a></td>
+        <td><a id="btnHomeToPharmacy" type="button" onclick="location.href='/pharmacy/'">지도-약국</a></td>
+        <td><a id="btnHomeToChat" type="button" onclick="location.href='/chat/'">Q&A</a></td>
+        <td><a id="btnMyPage" type="button" onclick="location.href='/profile/'">마이페이지</a></td>
+
+
+
+`;
+
+document.write(mainheader);
